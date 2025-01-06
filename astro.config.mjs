87e0e1b26c1feae4423a4c4509c12a212026b107
Afsin Kutluhan <afsinface11@yahoo.com>
2025-01-06 23:58:36 +0300

    Yaptığınız değişiklikleri açıklayan mesaj

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,25 @@ import vue from '@astrojs/vue';
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 
+import node from '@astrojs/node';
+
 export default defineConfig({
   prefetch: {
     defaultStrategy: 'viewport'
   },
+
   image: {
     domains: ["astro.build"],
   },
+
   site: 'https://bugunlerde.com',
   integrations: [vue(), tailwind(), icon(), sitemap()],
+
   build: {
     output: 'static',
   },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
