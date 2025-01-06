6a3330b14bc7232390dd4841cff06a19ccf3a354
code353535 <125445010+code353535@users.noreply.github.com>
2025-01-06 22:06:53 +0300

    d

--- trigger-build/index.js
+++ trigger-build/index.js
@@ -20,6 +20,6 @@ app.post("/trigger-build", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(4000, () => {
     console.log("Build trigger server is running on port 3000");
 });
