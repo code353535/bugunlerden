c66874fbbe1f31031bddce0b60c177e4e10a1285
code353535 <125445010+code353535@users.noreply.github.com>
2025-01-07 22:14:06 +0300

    b

--- trigger-build/deploy-build.sh
+++ trigger-build/deploy-build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash    
 
+# Ayarlar
+BUILD_DIR="/var/www/html/bugunlerden/temp"  # Temp klasörü yolu
+LIVE_DIR="/var/www/html/bugunlerden/dist"   # Dist klasörü yolu
+
 # 1. Yeni build oluştur
 echo "Running build in temporary directory..."
 npm ci
 npm run build && {
-# 2. change yap
+    # 2. Build tamamlandıktan sonra işlemi yap
     echo "Deploying new build to live site..."
-   rm -rf dist/* && mv temp/* dist/
+    rm -rf $LIVE_DIR/* && mv $BUILD_DIR/* $LIVE_DIR/
 } || {
     echo "Build failed."
     exit 1
 }
-# . Eski yedeği sakla veya temizle
+
+# 3. Eski yedeği sakla veya temizle
 echo "Deployment complete! Old site backed up at $BACKUP_DIR."
