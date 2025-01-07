5e157bb7ead97fef3f3e872794611ea547e3463a
code353535 <125445010+code353535@users.noreply.github.com>
2025-01-07 21:53:58 +0300

    e

--- trigger-build/deploy-build.sh
+++ trigger-build/deploy-build.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
 # Ayarlar
-BUILD_DIR="/var/www/html/bugunlerden/temp"           # Geçici build dizini
-LIVE_DIR="/var/www/html/bugunlerden/dist"        # Canlı site dizini
-BACKUP_DIR="/var/www/html/bugunlerden/backup"    # Yedekleme dizini
+BUILD_DIR="/var/www/html/bugunlerden/temp"       
+LIVE_DIR="/var/www/html/bugunlerden/dist"       
 
 echo "Starting build process..."
 
 # 1. Yeni build oluştur
 echo "Running build in temporary directory..."
 npm ci
-npm run build 
-
-# 2. Build işlemi tamamlandıktan sonra eski sürümü yedekle
-echo "Backing up current live site..."
-rm -rf $BACKUP_DIR
-mv $LIVE_DIR $BACKUP_DIR
-
-# 3. Yeni build'i canlı dizine taşı
-echo "Deploying new build to live site..."
-mv $BUILD_DIR/* $LIVE_DIR/
-
-# 4. Geçici klasörü temizle
-rm -rf $BUILD_DIR
-mkdir -p $BUILD_DIR
-
-# 5. Eski yedeği sakla veya temizle
+npm run build && {
+# 2. change yap
+    echo "Deploying new build to live site..."
+    mv $BUILD_DIR/* $LIVE_DIR/
+} || {
+    echo "Build failed."
+    exit 1
+}
+# . Eski yedeği sakla veya temizle
 echo "Deployment complete! Old site backed up at $BACKUP_DIR."
