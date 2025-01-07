011f3ec5ea8378bc8f8429257f6a2885705bb369
code353535 <125445010+code353535@users.noreply.github.com>
2025-01-07 19:21:31 +0300

    v

--- trigger-build/index.js
+++ trigger-build/index.js
@@ -6,17 +6,14 @@ app.use(express.json());
 
 app.post("/trigger-build", (req, res) => {
     console.log("Build Triggered!");
-    exec("npm run build", (error, stdout, stderr) => {
+    exec('node triggered-script.js', (error, stdout, stderr) => {
         if (error) {
-            console.error(`Error: ${error.message}`);
-            return res.status(500).send("Build failed");
+            console.error(`Script error: ${stderr}`);
+            res.status(500).send('Error running script');
+            return;
         }
-        if (stderr) {
-            console.error(`Stderr: ${stderr}`);
-            return res.status(500).send("Build failed");
-        }
-        console.log(`Stdout: ${stdout}`);
-        res.send("Build successful");
+        console.log(`Script output: ${stdout}`);
+        res.send('Script executed successfully');
     });
 });
 
