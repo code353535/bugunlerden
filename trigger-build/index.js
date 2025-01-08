ab7ee5b29fcf99052a66a38223599ef1912d06b0
code353535 <125445010+code353535@users.noreply.github.com>
2025-01-08 06:32:29 +0300

    bbb

--- trigger-build/index.js
+++ trigger-build/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { spawn } from "child_process";
-import cors from "cors";
+
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
 
 app.post("/trigger-build", (req, res) => {
     console.log("Build Triggered!");
