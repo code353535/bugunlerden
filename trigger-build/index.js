463d5eca7c24e66513ca7499001cac3140183f05
code353535 <125445010+code353535@users.noreply.github.com>
2025-01-20 21:40:10 +0300

    e

--- trigger-build/index.js
+++ trigger-build/index.js
@@ -9,36 +9,42 @@ app.use(express.json());
 // Job Queue oluşturuluyor
 const buildQueue = new Queue('buildQueue');
 
-// Build işlemini gerçekleştirecek worker
+// Worker yapılandırması
 const worker = new Worker('buildQueue', async job => {
     console.log('Astro.js build işlemi başlıyor...');
-    await new Promise((resolve, reject) => {
-        exec('npm run build', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Build hatası: ${stderr}`);
-                return reject(new Error('Build işlemi başarısız oldu'));
-            }
-            console.log('Build tamamlandı:\n', stdout);
-            resolve();
-        });
-    });
-
-    console.log('Dosya işlemleri başlıyor...');
-    await new Promise((resolve, reject) => {
-        exec(
-            'rm -rf /var/www/html/bugunlerden/dist/* && mv /var/www/html/bugunlerden/temp/* /var/www/html/bugunlerden/dist/',
-            (error, stdout, stderr) => {
+    
+    // Build işlemi
+    try {
+        await new Promise((resolve, reject) => {
+            exec('npm run build', (error, stdout, stderr) => {
                 if (error) {
-                    console.error(`Dosya işlemi hatası: ${stderr}`);
-                    return reject(new Error('Dosya işlemi başarısız oldu'));
+                    console.error(`Build hatası: ${stderr}`);
+                    return reject(new Error('Build işlemi başarısız oldu'));
                 }
-                console.log('Dosya işlemleri tamamlandı.');
+                console.log('Build tamamlandı:\n', stdout);
                 resolve();
-            }
-        );
-    });
+            });
+        });
 
-    console.log('Build ve dosya taşıma işlemleri tamamlandı.');
+        console.log('Dosya işlemleri başlıyor...');
+        await new Promise((resolve, reject) => {
+            exec(
+                'rm -rf /var/www/html/bugunlerden/dist/* && mv /var/www/html/bugunlerden/temp/* /var/www/html/bugunlerden/dist/',
+                (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`Dosya işlemi hatası: ${stderr}`);
+                        return reject(new Error('Dosya işlemi başarısız oldu'));
+                    }
+                    console.log('Dosya işlemleri tamamlandı.');
+                    resolve();
+                }
+            );
+        });
+
+        console.log('Build ve dosya taşıma işlemleri tamamlandı.');
+    } catch (error) {
+        console.error('Hata:', error.message);
+    }
 });
 
 // Webhook tetikleme route'u
